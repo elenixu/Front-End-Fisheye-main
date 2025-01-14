04a670b9767d82aed2d88d5f3c446930757e24d7
Elena <elegil93@gmail.com>
2025-01-14 07:29:19 +0100

    update 2

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -88,4 +88,21 @@ async function initPhotographerPage() {
   renderPhotographerMedia(photographerMedia)
 }
 
+const $modal = document.getElementById('contactModal')
+const $body = document.body
+
+function displayModal() {
+  $modal.setAttribute('aria-hidden', 'false')
+  $body.classList.add('no-scroll')
+  $modal.style.display = 'flex'
+  $modal.querySelector('.modal-close').focus()
+}
+
+function closeModal() {
+  $modal.setAttribute('aria-hidden', 'true')
+  $body.classList.remove('no-scroll')
+  $modal.style.display = 'none'
+  document.querySelector('.contact_button').focus()
+}
+
 document.addEventListener('DOMContentLoaded', initPhotographerPage)
